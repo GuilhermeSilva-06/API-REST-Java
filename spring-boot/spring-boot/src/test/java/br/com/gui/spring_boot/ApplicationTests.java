9b1b00bef36d37e9cd538c4b6773e1b9fa737a96
Guilherme Silva <gui.silva021007@gmail.com>
2026-09-02 15:44:23 -0300

    Projeto criado

---
+++ spring-boot/spring-boot/src/test/java/br/com/gui/spring_boot/ApplicationTests.java
@@ -0,0 +1,14 @@
+/* package br.com.gui.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
